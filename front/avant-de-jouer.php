c46e3e830e718c407d66d92f903ccaa87c2e2424
Yoogo9z <hugocuenot@live.fr>
2024-07-26 14:43:32 +0200

    Page avant-de-jouer faite

--- front/avant-de-jouer.php
+++ front/avant-de-jouer.php
@@ -19,24 +19,50 @@
         <div class="gauche-et-droite">
             <div class="gauche">
                 <div class="joueurs">
-                    <h2>Joueurs</h2>
+                    <h2>Joueurs </h2>
+                    <h2>8</h2>
                 </div>
-                <div class="list-joueurs"></div>
+                <div class="list-joueurs">
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                    <div class="joueur"><p>Bouzin malicieux</p></div>
+                </div>
+                <a href="./index.php"><button class="retour lien"><i class="fa-solid fa-angle-left"></i>
+                        <p class="txt-connexion">Inviter des amis</p>
+                    </button></a>
             </div>
             <div class="droite">
                 <div class="Thèmes">
                     <h2>Thèmes</h2>
                 </div>
-                <div class="list-joueurs"></div>
+                <div class="list-quiz">
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                    <div class="carte-quiz"><i class="fa-solid fa-basketball"></i> <p class="titre-carte">Sport</p></div>
+                </div>
+                <a href="./index.php" class="btn-droite"><button class="retour lancer-quiz"><i class="fa-solid fa-angle-left"></i>
+                        <p class="txt-connexion">Lancer le quiz</p>
+                    </button></a>
             </div>
         </div>
         <div class="deux-btn">
-            <a href="./index.php"><button class="retour"><i class="fa-solid fa-angle-left"></i>
-                    <p class="txt-connexion">Inviter des amis</p>
-                </button></a>
-            <a href="./index.php"><button class="retour"><i class="fa-solid fa-angle-left"></i>
-                    <p class="txt-connexion">Lancer le quiz</p>
-                </button></a>
         </div>
     </div>
 </body>
