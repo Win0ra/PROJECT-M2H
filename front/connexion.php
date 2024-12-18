ea3e13fe69a5e2aff7f294edbc88449026ff1edf
Hugo Cnt <hugocuenot@live.fr>
2024-12-18 21:24:26 +0100

    Mot de passe oublié

--- front/connexion.php
+++ front/connexion.php
@@ -18,13 +18,13 @@ include './navbar.php';
         <h1 class="h1-connex">Connectez-vous</h1>
         <h3>Email</h3>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-            <input type="text" name="email" id="email" required>
+            <input type="text" name="email" id="email" required placeholder="MonsieurBouzin@gmail.com">
             <h3>Mot de passe</h3>
-            <input type="password" name="password" id="password" required>
+            <input type="password" name="password" id="password" required placeholder="****************">
             <div class="checkbox">
                 <input type="checkbox" name="checkbox" id="checkbox">
                 <p class="memorize">Se souvenir de moi</p>
-                <p class="password-lost"><a href="">Mot de passe oublié ?</a></p>
+                <p class="password-lost"><a href="./forget-password.php">Mot de passe oublié ?</a></p>
             </div>
             <button class="connexion" type="submit">
                 <i class="fa-solid fa-user"></i>
@@ -49,20 +49,24 @@ include './navbar.php';
         try {
             $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            if (isset($_POST['email'])) {
+            if (isset($_POST['email'], $_POST['password'])) {
                 $email = $_POST['email'];
                 $stmt = $pdo->prepare('SELECT * FROM useridentified WHERE email = :email');
                 $stmt->bindValue(':email', $email);
                 $stmt->execute();
                 $useridentified = $stmt->fetch(PDO::FETCH_ASSOC);
-                if ($useridentified && $_POST['password'] === $useridentified['password']) {
-                    $_SESSION['user_id'] = $useridentified['id'];
-                    $_SESSION['user_last_name'] = $useridentified['last_name'];
-                    $_SESSION['user_first_name'] = $useridentified['first_name'];
-                    header('Location: register.php');
-                    exit();
+                if ($useridentified) {
+                    if (password_verify($_POST['password'], $useridentified['password'])) {
+                        $_SESSION['user_id'] = $useridentified['id'];
+                        $_SESSION['user_last_name'] = $useridentified['last_name'];
+                        $_SESSION['user_first_name'] = $useridentified['first_name'];
+                        header('Location: register.php');
+                        exit();
+                    } else {
+                        echo 'Adresse e-mail ou mot de passe incorrect.';
+                    }
                 } else {
-                    echo 'Adresse e-mail ou mot de passe incorrect.';
+                    echo 'Aucun utilisateur trouvé avec cet e-mail.';
                 }
             }
         } catch (PDOException $e) {
